e581356e0228f0ba8a316ccc832ec15cf8406699
CoderBDK <cobdgithub07@gmail.com>
2023-10-08 17:42:59 +0100

    preload - splash screen added

--- app/src/main/java/com/coderbdk/bazardor/ui/main/MainActivity.kt
+++ app/src/main/java/com/coderbdk/bazardor/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.coderbdk.bazardor
+package com.coderbdk.bazardor.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.coderbdk.bazardor.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
