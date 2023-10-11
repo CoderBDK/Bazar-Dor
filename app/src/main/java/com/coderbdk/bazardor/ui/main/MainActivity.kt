aec580432838e6c2f58d24254490d74cdc63d1ba
CoderBDK <cobdgithub07@gmail.com>
2023-10-11 13:23:31 +0100

    main layout - ui design completed

--- app/src/main/java/com/coderbdk/bazardor/ui/main/MainActivity.kt
+++ app/src/main/java/com/coderbdk/bazardor/ui/main/MainActivity.kt
@@ -3,10 +3,14 @@ package com.coderbdk.bazardor.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coderbdk.bazardor.R
+import com.coderbdk.bazardor.databinding.ActivityMainAppBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_app)
+        val binding = ActivityMainAppBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.appBarMain.toolbar)
+
     }
 }
